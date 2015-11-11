aa2964b60bd340be51322577392dcd84c47d6b6c
svanlaere <svanlaere@users.noreply.github.com>
2015-11-11 12:33:11 +0100

    Update nl-message.php

--- themer/i18n/nl-message.php
+++ themer/i18n/nl-message.php
@@ -1,16 +1,34 @@
 <?php
+/*
+ |	Themer i18n Translation File
+ |	@file		./i18n/nl-message.php
+ |	@version	1.0.0 - Stable
+ |	@creator	pawDEV I18N Creator Tool
+ |				by pytesNET
+ */
+	if(!defined("IN_CMS")){ die(); }
 
-    /**
-     * Dutch i18n
-     */
+	$return = array();
 
-    return array(
-	'Wordpress admin theme' => 'Wordpress admin thema',
-	'Settings for Wordpress admin theme' => 'Instellingen voor Wordpress admin thema',
-	'Sidebar width' => 'Zijbalk breedte',
-	'Set your desired value in pixels. Default value: 180' => 'Geef de gewenste pixelwaarde in. Standaard waarde: 180',
-	'Color scheme' => 'Kleurenschema',
-	'Set your favorite colorscheme. Default value: Default' => 'Kies je favoriete kleurscheme. Standaard waarde: Default',
-	'This plugin requires the :themename admin theme' => 'Deze plugin vereist het :themename admin thema',
-	'This plugin requires PHP 5.3 or higher' => 'This plugin vereist PHP 5.3 of hoger'
-	);
+	$return["This plugin requires a pawedWolf admin theme!"] = "Deze plugin vereist een pawedWolf admin thema";
+	$return["This plugin requires PHP 5.3 or higher!"] = "Deze plugin vereist PHP 5.3 of hoger!";
+	$return["A configuration plugin for the pawedWolf admin themes."] = "Een configuratie plugin voor de pawedWolf admin themas";
+	$return["pawedWolf Admin Themes"] = "pawedWolf Admin Themas";
+	$return["Could not save settings, no settings found."] = "Kon de instellingen niet bewaren.";
+	$return["The settings have been saved."] = "De instellingen zijn bewaard";
+	$return["An error occured trying to save the settings."] = "Er is een fout opgetreden tijdens het bewaren van de instellingen.";
+	$return["No pawedWolf admin theme selected!"] = "Er is geen pawedWolf admin thema geselecteerd!";
+	$return["Select an admin theme"] = "Selecteer een admin thema";
+	$return["No pawedWolf admin theme found!"] = "Er is geen pawedWolf admin thema gevonden!";
+	$return["Set admin theme"] = "Gebruik admin thema";
+	$return["Settings for the :theme admin theme"] = "Instellingen voor de :theme admin thema";
+	$return["Sidebar width"] = "Zijbalk breedte";
+	$return["Set your desired value in pixels. Default value: 180"] = "Geef de gewenste pixelwaarde in. Standaard waarde: 180";
+	$return["Color scheme"] = "Kleurenschema";
+	$return["Set your favorite colorscheme. Default value: Default"] = "Kies je favoriete kleurscheme. Standaard waarde: Default";
+	$return["Responsive Design"] = "Responsive Design";
+	$return["Enable"] = "Activeren";
+	$return["Save Settings"] = "Bewaar instellingen";
+	$return["Reset Settings"] = "Reset instellingen";
+
+	return $return;
